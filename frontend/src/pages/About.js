1cf2f03328543fc5c223a4e9aba21c5cd46ba27e
roger melo <meloroger059@gmail.com>
2019-07-15 12:48:43 +0000

    link added to repo

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -8,7 +8,14 @@ const About = () => {
         I built this Web App using React.js and the Contracts are written in
         Solidity. Please visit my repository if you would like to see my code.
       </p>
-      <button className='btn waves blue darken-3 btn-large'>Repository</button>
+      <a
+        href='https://github.com/meloroger/Terca-Token.git'
+        target='_blank'
+        rel='noopener noreferrer'>
+        <button className='btn waves blue darken-3 btn-large'>
+          Repository
+        </button>
+      </a>
     </div>
   );
 };
